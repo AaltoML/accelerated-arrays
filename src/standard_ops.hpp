1a3237c841aa677939543eeae4c63d2288c3f350
Otto Seiskari <otto.seiskari@gmail.com>
2020-10-13 13:42:59 +0300

    Fix operation specs: return copy in builders to avoid references to temporary objects

--- src/standard_ops.hpp
+++ src/standard_ops.hpp
@@ -10,11 +10,12 @@ namespace operations {
 namespace fill {
     struct Spec {
         std::vector<double> value;
-        Spec &setValue(const std::vector<double> &v) {
+        Spec setValue(const std::vector<double> &v) {
             value = v;
             return *this;
         }
-        template <class V> Spec &setValue(V v) {
+
+        template <class V> Spec setValue(V v) {
             value = { static_cast<double>(v) };
             return *this;
         }
@@ -35,35 +36,35 @@ namespace fixedConvolution2D {
 
         Image::Border border = Image::Border::ZERO;
 
-        Spec &setKernel(const std::vector< std::vector<double> > &k, double scale = 1.0) {
+        Spec setKernel(const std::vector< std::vector<double> > &k, double scale = 1.0) {
             kernel = k;
             for (auto &row : kernel)
                 for (auto &el : row) el *= scale;
             return *this;
         }
 
-        Spec &setBias(double b) {
+        Spec setBias(double b) {
             bias = b;
             return *this;
         }
 
-        Spec &setStride(int x, int y) {
+        Spec setStride(int x, int y) {
             xStride = x;
             yStride = y;
             return *this;
         }
 
-        Spec &setStride(int xy) {
+        Spec setStride(int xy) {
             return setStride(xy, xy);
         }
 
-        Spec &setOffset(int x, int y) {
+        Spec setOffset(int x, int y) {
             xOffset = x;
             yOffset = y;
             return *this;
         }
 
-        Spec &setBorder(Image::Border b) {
+        Spec setBorder(Image::Border b) {
             border = b;
             return *this;
         }
@@ -90,14 +91,14 @@ namespace pixelwiseAffine {
         // empty means no bias
         std::vector< double > bias;
 
-        Spec &setLinear(const std::vector< std::vector<double> > &matrix, double scale = 1.0) {
+        Spec setLinear(const std::vector< std::vector<double> > &matrix, double scale = 1.0) {
             linear = matrix;
             for (auto &row : linear)
                 for (auto &el : row) el *= scale;
             return *this;
         }
 
-        Spec &setBias(const std::vector<double> &b) {
+        Spec setBias(const std::vector<double> &b) {
             bias = b;
             return *this;
         }
@@ -114,12 +115,12 @@ namespace channelwiseAffine {
         double scale = 1.0;
         double bias = 0.0;
 
-        Spec &setScale(double a) {
+        Spec setScale(double a) {
             scale = a;
             return *this;
         }
 
-        Spec &setBias(double b) {
+        Spec setBias(double b) {
             bias = b;
             return *this;
         }
